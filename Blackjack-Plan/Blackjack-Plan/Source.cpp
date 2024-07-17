062c7b092d88f46dbb22be6fe24cfa2d9380c1c8
Fred Burdekin <FRED.BURDEKIN22@bathspa.ac.uk>
2024-07-17 18:38:22 +0100

    Created a setup function
    Created a setup function that will run to setup the code for the game of blackjack

--- Blackjack-Plan/Blackjack-Plan/Source.cpp
+++ Blackjack-Plan/Blackjack-Plan/Source.cpp
@@ -1,10 +1,21 @@
 #include <iostream> 
-
+#include <vector>
 
 using namespace std;
 
 
+void setup(bool& gamePlaying, vector<int>& cards, int& player1Hand, int& player2Hand, int& player3Hand) {
+	cards = { 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+	gamePlaying = true; // Set to true to start the game 
+	player1Hand = 0;
+	player2Hand = 0;
+	player3Hand = 0;
+
+}
+
+
 int main() {
+
 
 
 

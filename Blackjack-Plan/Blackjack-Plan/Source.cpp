90013c434a2da38cf0f4c2707ec3d63c3ce343cf
Fred Burdekin <FRED.BURDEKIN22@bathspa.ac.uk>
2024-07-17 18:58:17 +0100

    Started creating the main function

--- Blackjack-Plan/Blackjack-Plan/Source.cpp
+++ Blackjack-Plan/Blackjack-Plan/Source.cpp
@@ -16,9 +16,39 @@ void setup(bool& gamePlaying, vector<int>& cards, int& player1Hand, int& player2
 
 int main() {
 
+	// Defining the variables
+	bool dealing, player1Drawing = true, player2Drawing = true, player3Drawing = true, player1Bust = false, player2Bust = false, player3Bust = false;
+	vector<int> cards;
+	int player1Hand, player2Hand, player3Hand, userInput, draw;
+
+	// call setup function
+	setup(dealing, cards, player1Hand, player2Hand, player3Hand);
+
+	// Initialize random seed 
+	srand(static_cast<unsigned>(time(0)));
 
 
 
+    if (dealing) {
+        for (int i = 0; i < 2; i++) {
+            draw = rand() % cards.size();
+            player1Hand += cards[draw];
+            cards.erase(cards.begin() + draw);
 
+            draw = rand() % cards.size();
+            player2Hand += cards[draw];
+            cards.erase(cards.begin() + draw);
+
+            draw = rand() % cards.size();
+            player3Hand += cards[draw];
+            cards.erase(cards.begin() + draw);
+        }
+
+        cout << "Starting Hand for player 1: " << player1Hand << endl;
+        cout << "Starting Hand for player 2: " << player2Hand << endl;
+        cout << "Starting Hand for player 3: " << player3Hand << endl;
+
+        dealing = false;
+    }
 	return 0;
 }

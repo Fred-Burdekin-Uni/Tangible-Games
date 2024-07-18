edaad2274af2a576d91f3ebdbf09261b22494dc5
Fred Burdekin <FRED.BURDEKIN22@bathspa.ac.uk>
2024-07-18 19:17:18 +0100

    Created the loop function

--- Blackjack/Blackjack.ino
+++ Blackjack/Blackjack.ino
@@ -33,4 +33,64 @@ void setup() {
   rfid.PCD_Init(); // initialize rfid reader 
   initializeCards(); // initialize the cards array 
   randomSeed(analogRead(0)); // initialize random seed 
-} 
+}
+
+
+void loop(){
+  if (gamePlaying) { 
+      // handle game logic 
+    if (player1Drawing) { 
+      playerTurn(1, player1Hand, player1Drawing, player1Bust); 
+    } else if (player2Drawing) {
+      playerTurn(2, player2Hand, player2Drawing, player2Bust); 
+    } else if (player3Drawing) { 
+      playerTurn(3, player3Hand, player3Drawing, player3Bust); 
+    } else { 
+      determineWinner(); // determine the winner when all players are done 
+      gamePlaying = false; // end game 
+      waitingForRestart = true; // wait for restart input 
+    } 
+  } else if (waitingForRestart) { 
+    // check for restart input 
+    Serial.println("would you like to play again? (1 = yes, 0 = no)"); 
+    while (Serial.available() == 0) { 
+    // wait for input 
+    } 
+    int userInput = Serial.parseInt(); 
+    if (userInput == 1) { 
+      resetGame(); // restart the game 
+    } else if (userInput = 0) { 
+      Serial.println("game over."); 
+      while (true); // stop execution 
+    } else { 
+      Serial.println("invalid input. please enter 1 to restart or 0 to end."); 
+    } 
+  } else { 
+    // wait for player identification to start the game 
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) { 
+      // check if the detected uid matches any player's uid 
+      if (isSameUID(rfid.uid.uidByte, player1UID)) { 
+        Serial.println("player 1 identified"); 
+        player1Hand = 0; // reset player 1 hand 
+        player1Drawing = true; // allow player 1 to draw 
+      } else if (isSameUID(rfid.uid.uidByte, player2UID)) { 
+        Serial.println("player 2 identified"); 
+        player2Hand = 0; // reset player 2 hand 
+        player2Drawing = true; // allow player 2 to draw 
+      } else if (isSameUID(rfid.uid.uidByte, player3UID)) { 
+        Serial.println("player 3 identified"); 
+        player3Hand = 0; // reset player 3 hand 
+        player3Drawing = true; // allow player 3 to draw 
+      } else { 
+        Serial.println("unknown player"); 
+      } 
+      rfid.PICC_HaltA(); // halt rfid card detection 
+      rfid.PCD_StopCrypto1(); // stop encryption on rfid reader 
+
+      if (!gamePlaying) { 
+        gamePlaying = true; // start the game 
+        dealInitialCards(); // deal initial cards to players 
+       
+    } 
+  } 
+}
